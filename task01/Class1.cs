5f38f7a9c0b398448473f5d51f15f730a0b38ce4
cmexoroma2007 <mr.rol228228@gmail.com>
2026-06-23 17:24:55 +0600

    Add: IsPalindrome extensions

--- task01/Class1.cs
+++ task01/Class1.cs
@@ -1,6 +1,30 @@
 ﻿namespace task01;
 
-public class Class1
+public static class StringExtensions
 {
+    public static bool IsPalindrome(this string input)
+    {
+        if(input.Length == 0) return false;
+        
+        input = input.ToLower();
+        int leftIndex = 0;
+        int rightIndex = input.Length - 1;
+        while(leftIndex < rightIndex)
+        {
+            while(leftIndex < rightIndex && (char.IsPunctuation(input[leftIndex]) || char.IsWhiteSpace(input[leftIndex])))
+            {
+                leftIndex += 1;
+            }
+            while(leftIndex < rightIndex && (char.IsPunctuation(input[rightIndex]) || char.IsWhiteSpace(input[rightIndex])))
+            {
+                rightIndex -= 1;
+            }
 
+            if(input[leftIndex] != input[rightIndex]) return false;
+
+            leftIndex += 1;
+            rightIndex -= 1;
+        }
+        return true;
+    }
 }
