16632e978d8d4be342ebc8354197101b9be354ae
cmexoroma2007 <mr.rol228228@gmail.com>
2026-06-26 18:13:42 +0600

    Add: realization

--- task05tests/UnitTest1.cs
+++ task05tests/UnitTest1.cs
@@ -1,10 +1,65 @@
-﻿namespace task05tests;
+﻿using Xunit;
+using Moq;
+using task05;
 
-public class UnitTest1
+namespace task05tests;
+
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField;
+    public int Property { get; set; }
+
+    public void Method() { }
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
 {
     [Fact]
-    public void Test1()
+    public void GetPublicMethods_ReturnsCorrectMethods()
     {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
 
+        Assert.Contains("Method", methods);
+    }
+
+    [Fact]
+    public void GetMethodParams_ReturnCorrectParams()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var param = analyzer.GetMethodParams("Method");
+
+        Assert.Contains("Void", param);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+
+        Assert.Contains("_privateField", fields);
+    }
+
+    [Fact]
+    public void GetProperties_ReturnCorrectProperies()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var prop = analyzer.GetProperties();
+
+        Assert.Contains("Property", prop);
+    }
+
+    [Fact]
+    public void HasAttribute_ReturnTrue()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+        bool res = analyzer.HasAttribute<SerializableAttribute>();
+
+        Assert.True(res);
     }
 }
