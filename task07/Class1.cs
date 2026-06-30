d57aa7ed33ef84fe7f37c25f8b703fd394f78e4d
cmexoroma2007 <mr.rol228228@gmail.com>
2026-06-30 18:49:15 +0600

    Add: DisplayNameAttribute, VersionAttribute, SampleClass, PrintTypeInfo, Tests

--- task07/Class1.cs
+++ task07/Class1.cs
@@ -1,6 +1,62 @@
-﻿namespace task07;
+﻿using System.Reflection;
 
-public class Class1
+namespace task07;
+
+[System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Method | System.AttributeTargets.Property)]
+public class DisplayNameAttribute : Attribute
 {
+    public string DisplayName { get; }
 
+    public DisplayNameAttribute(string displayName)
+    {
+        DisplayName = displayName;
+    }
+}
+
+[System.AttributeUsage(System.AttributeTargets.Class)]
+public class VersionAttribute : Attribute
+{
+    public int Major { get; }
+    public int Minor { get; }
+
+    public VersionAttribute(int major, int minor)
+    {
+        Major = major;
+        Minor = minor;
+    }
+}
+
+[DisplayName("Пример класса")]
+[Version(1, 0)]
+public class SampleClass
+{
+    [DisplayName("Числовое свойство")]
+    public int Number { get; set; }
+
+    [DisplayName("Тестовый метод")]
+    public void TestMethod() {}
+}
+
+public static class ReflectionHelper
+{
+    public static void PrintTypeInfo(this Type type)
+    {
+        var classDispNameAttr = type.GetCustomAttribute<DisplayNameAttribute>();
+        if(classDispNameAttr != null) Console.WriteLine($"Имя класса: {classDispNameAttr.DisplayName}");
+
+        var classVersion = type.GetCustomAttribute<VersionAttribute>();
+        if(classVersion != null) Console.WriteLine($"Версия класса: {classVersion.Major}.{classVersion.Minor}");
+
+        foreach(var method in type.GetMethods())
+        {
+            var methodDispNameAttr = method.GetCustomAttribute<DisplayNameAttribute>();
+            if(methodDispNameAttr != null) Console.WriteLine($"Класс имеет метод: {method.Name} - {methodDispNameAttr.DisplayName}");
+        }
+
+        foreach(var prop in type.GetProperties())
+        {
+            var propDispNameAttr = prop.GetCustomAttribute<DisplayNameAttribute>();
+            if(propDispNameAttr != null) Console.WriteLine($"Класс имеет свойство: {prop.Name} - {propDispNameAttr.DisplayName}");
+        }
+    }
 }
--- task07tests/UnitTest1.cs
+++ task07tests/UnitTest1.cs
@@ -1,10 +1,67 @@
-﻿namespace task07tests;
+﻿using Xunit;
+using task07;
+using System.Reflection;
 
-public class UnitTest1
+
+namespace task07tests;
+
+public class AttributeReflectionTests
 {
     [Fact]
-    public void Test1()
+    public void Class_HasDisplayNameAttribute()
     {
+        var type = typeof(SampleClass);
+        var attribute = type.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Пример класса", attribute.DisplayName);
+    }
 
+    [Fact]
+    public void Method_HasDisplayNameAttribute()
+    {
+        var method = typeof(SampleClass).GetMethod("TestMethod");
+        var attribute = method.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Тестовый метод", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Property_HasDisplayNameAttribute()
+    {
+        var prop = typeof(SampleClass).GetProperty("Number");
+        var attribute = prop.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Числовое свойство", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Class_HasVersionAttribute()
+    {
+        var type = typeof(SampleClass);
+        var attribute = type.GetCustomAttribute<VersionAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal(1, attribute.Major);
+        Assert.Equal(0, attribute.Minor);
+    }
+
+    [Fact]
+    public void PrintTypeInfo_LogTest()
+    {
+        var type = typeof(SampleClass);
+        var output = new StringWriter();
+
+        Console.SetOut(output);
+
+        var expectedString1 = "Имя класса: Пример класса";
+        var expectedString2 = "Версия класса: 1.0";
+        var expectedString3 = "Класс имеет метод: TestMethod - Тестовый метод";
+        var expectedString4 = "Класс имеет свойство: Number - Числовое свойство";
+
+        type.PrintTypeInfo();
+        
+        Assert.Contains(expectedString1, output.ToString());
+        Assert.Contains(expectedString2, output.ToString());
+        Assert.Contains(expectedString3, output.ToString());
+        Assert.Contains(expectedString4, output.ToString());
     }
 }
