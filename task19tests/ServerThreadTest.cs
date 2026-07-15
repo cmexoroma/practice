9111a980ee7058d98a984d6305962ba3c3944d58
cmexoroma2007 <mr.rol228228@gmail.com>
2026-07-15 19:03:26 +0600

    create report, graph

--- task19tests/ServerThreadTest.cs
+++ task19tests/ServerThreadTest.cs
@@ -2,6 +2,8 @@
 using System.Threading;
 using Moq;
 using Xunit;
+using ScottPlot;
+using System.Text.RegularExpressions;
 
 namespace task19tests;
 
@@ -17,7 +19,7 @@ public class ServerThreadTest
         Console.SetOut(output);
 
         for (int i = 1; i < 6; i++) server.EnqueueCommand(new TestCommand(i));
-        
+
         thread.Start();
         Thread.Sleep(500);
 
@@ -26,6 +28,7 @@ public class ServerThreadTest
         thread.Join(500);
 
         var str = output.ToString().Replace("\r", "").Split("\n").ToList();
+        foreach(var s in str) Console.WriteLine(s);
         str.Remove("");
         var ex = new List<string>();
         for (int i = 1; i < 4; i++)
@@ -37,5 +40,40 @@ public class ServerThreadTest
         }
         Assert.Equal(ex, str);
         Assert.False(thread.IsAlive);
+
+        using (var writer = new StreamWriter(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "report.txt")))
+        {
+            if (ex.SequenceEqual(str)) writer.WriteLine("Задачи выполняются в верной последовательности");
+            else writer.WriteLine("Задачи выполняются в неверной последовательности");
+        }
+
+        var globalSteps = 0;
+        var executionLog = new List<(double Step, double TaskId)>();
+
+        foreach (var l in str)
+        {
+            var mathces = Regex.Matches(l, @"\d+");
+
+            if (mathces.Count >= 2)
+            {
+                globalSteps++;
+                executionLog.Add((globalSteps, double.Parse(mathces[0].Value)));
+            }
+        }
+
+        var plt = new Plot();
+
+        var steps = executionLog.Select(x => x.Step).ToArray();
+        var taskIds = executionLog.Select(x => x.TaskId).ToArray();
+
+        var scatter = plt.Add.Scatter(steps, taskIds);
+        scatter.LineWidth = 0;
+
+        plt.Title("Сетка выполнения задач (Round Robin)");
+        plt.XLabel("Сквозной шаг выполнения сервера");
+        plt.YLabel("ID задачи");
+        plt.Axes.Left.SetTicks(new double[] {1, 2, 3, 4, 5}, new string[] {"1", "2", "3", "4", "5"});
+
+        plt.SavePng(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "round_robin.png"), 800, 600);
     }
 }
